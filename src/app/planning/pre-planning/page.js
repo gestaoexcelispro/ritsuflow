ef56853ef319f0059cb0645f5a5310cf270e59d7
gestaoexcelispro <eduardo.freitas@excelispro.com>
2026-09-08 14:06:28 -0300

    Update page.js

--- src/app/planning/pre-planning/page.js
+++ src/app/planning/pre-planning/page.js
@@ -1,33 +1,13 @@
-import {
-  cloneElement,
-  isValidElement,
-} from 'react'
-
-import DashboardPrePlanningPage from '../../dashboard/planning/pre-planning/page'
+import PrePlanningPage from '../../dashboard/planning/pre-planning/page'
 
 
 export default async function StandalonePrePlanningPage({
   searchParams,
 }) {
-  const content =
-    await DashboardPrePlanningPage({
-      searchParams,
-    })
-
-  if (
-    !isValidElement(
-      content
-    )
-  ) {
-    return content
-  }
-
-  return cloneElement(
-    content,
-    {
-      standalone: true,
-      changeProjectHref:
-        '/planning/pre-planning',
-    }
-  )
+  return PrePlanningPage({
+    searchParams,
+    standalone: true,
+    changeProjectHref:
+      '/planning/pre-planning',
+  })
 }
